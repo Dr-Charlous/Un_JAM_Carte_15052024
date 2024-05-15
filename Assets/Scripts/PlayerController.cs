d3a376d36915ebe65b84acd7a109e96a128fce6c
P3D\iris.pecquet <irispecquet@icloud.com>
2024-05-15 11:05:58 +0200

    Player Controller controls cards (drag and drop)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+    private Card _currentCard;
+    private Vector2 _currentMousePosWorld;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            InitDrag();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Drag();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            EndDrag();
+        }
+    }
+    
+    private void InitDrag()
+    {
+        _currentMousePosWorld = _camera.ScreenToWorldPoint(Input.mousePosition);
+
+        RaycastHit2D hit = Physics2D.Raycast(_currentMousePosWorld, Vector2.zero);
+
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.name);
+            if (hit.collider.TryGetComponent(out Card card))
+            {
+                _currentCard = card;
+            }
+        }
+    }
+
+    private void Drag()
+    {
+        _currentMousePosWorld = _camera.ScreenToWorldPoint(Input.mousePosition);
+        
+        if (_currentCard != null)
+        {
+            _currentCard.transform.position = _currentMousePosWorld;
+        }
+    }
+    
+    private void EndDrag()
+    {
+        _currentCard = null;
+    }
+}
